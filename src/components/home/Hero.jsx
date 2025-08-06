b7acbcf092391415e1b4065b0f6ce26b0892832f
Hrishikesh Yashwant Joshi <hrishi@Hrishikeshs-iMac-2.local>
2025-08-06 18:32:31 +0530

    first phase update 1

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -20,6 +20,11 @@ export default function Hero() {
 		const maskedBack = document.getElementById("masked-back");
 		const backGradient = document.getElementById("back-gradient");
 		const backShadow = document.getElementById("back-shadow");
+		const bgAnimTop = document.getElementById("bg-anim-top");
+		const bgAnimBottom = document.getElementById("bg-anim-bottom");
+		const animPointWrapper = document.getElementById("anim-points-wrapper");
+		const landingPoint1 = document.getElementById("landing-point-1");
+		const landingPoint2 = document.getElementById("landing-point-2");
 		// maskedBack.style.width = containerWidth;
 		// maskedBack.style.height = containerHeight;
 
@@ -34,15 +39,18 @@ export default function Hero() {
 				end: '+=100%', // or use pixel value like '+=800'
 				scrub: true,
 				pin: true,
-				anticipatePin: 1
+				// anticipatePin: 1,
+				once: true 
 			},
 		});
 
 		
-		tl.to(rectRef.current, {
+		tl
+		.to(rectRef.current, {
 			x: "+=7.2%",
 			duration: 0.2
-		}).to(maskedBack, {
+		})
+		.to(maskedBack, {
 			x: "+=10%",
 			y: "4%",
 			rotation: 20,
@@ -52,8 +60,10 @@ export default function Hero() {
 		}, "<")
 		.to(backGradient, {
 			width: "10%",
+			opacity: 1,
 			duration: 0.2,
 		}, "<");
+		
 
 		tl.add([
 			gsap.to(rectRef.current, {
@@ -69,6 +79,7 @@ export default function Hero() {
 			}),
 			gsap.to(backGradient, {
 				width: "40%",
+				opacity: 1,
 				duration: 2,
 			}),
 		])
@@ -92,18 +103,58 @@ export default function Hero() {
 			})
 		]);
 
+		tl.add([
+			gsap.from(animPointWrapper, {
+				height: 0,
+				duration: 1,
+				delay: 1
+			}),
+		]);
+
+		tl.add([
+			gsap.to(landingPoint1, {
+				scale: 1,
+				duration: 1,
+				delay: 2
+			}),
+		]);
+		
+		tl.add([
+			gsap.to(landingPoint2, {
+				scale: 1,
+				duration: 1,
+				delay: 2
+			}),
+			gsap.to(bgAnimTop, {
+				scale: 0,
+				opacity: 0,
+				duartion: 1,
+				delay: 3,
+			}),
+			gsap.to(bgAnimBottom, {
+				scale: 0,
+				opacity: 0,
+				duartion: 1,
+				delay: 3,
+			})
+		]);
+
 	}, { scope: containerRef });
 	
 
 	return (
 		<div className='w-full' style={{ height: "calc(var(--spacing-unit) * 18)", paddingTop: "var(--spacing-unit)" }} ref={containerRef}>
 			<div className='relative w-full' style={{ height: "calc(var(--spacing-unit) * 18)" }}>
-				<div className='w-full' style={{ height: "calc(var(--spacing-unit) * 12)", backgroundImage: "url(/assets/cont/home/hero/db-top.webp)", backgroundSize: "cover", backgroundPosition: "center bottom", backgroundRepeat: "no-repeat" }}>
-					{/* <Image src="/assets/cont/home/hero/db-top.webp" width={2400} height={1042} alt="" className='inline-block w-full' /> */}
+				<div className='w-full' id='bg-anim-top' style={{ height: "calc(var(--spacing-unit) * 12)", backgroundImage: "url(/assets/cont/home/hero/db-top.webp)", backgroundSize: "cover", backgroundPosition: "center bottom", backgroundRepeat: "no-repeat" }}></div>
+				<div className='w-full flex bg-white' id='anim-points-wrapper' style={{ height: "calc(var(--spacing-unit) * 8)" }}>
+					<div className="flex-1 flex items-center justify-center">
+						<Image src={"/assets/cont/home/hero/one.webp"} id="landing-point-1" width={1002} height={300} alt='test' className='inline-block' style={{ height: "calc(var(--spacing-unit) * 3)", width: "auto", transform: "scale(0)" }} />
+					</div>
+					<div className="flex-1 flex items-center justify-center" id="lading-point-2">
+						<Image src={"/assets/cont/home/hero/two.webp"} id="landing-point-2" width={1002} height={300} alt='test' className='inline-block' style={{ height: "calc(var(--spacing-unit) * 3)", width: "auto", transform: "scale(0)" }} />
+					</div>
 				</div>
-				<div className='w-full' style={{ height: "calc(var(--spacing-unit) * 6)", backgroundImage: "url(/assets/cont/home/hero/db-bottom.webp)", backgroundSize: "cover", backgroundPosition: "center top", backgroundRepeat: "no-repeat" }}>
-					{/* <Image src="/assets/cont/home/hero/db-bottom.webp" width={2400} height={1042} alt="" className='inline-block w-full' /> */}
-				</div>
+				<div className='w-full' id="bg-anim-bottom" style={{ height: "calc(var(--spacing-unit) * 6)", backgroundImage: "url(/assets/cont/home/hero/db-bottom.webp)", backgroundSize: "cover", backgroundPosition: "center top", backgroundRepeat: "no-repeat" }}></div>
 			</div>
 			<div className="innerContainer w-full h-full absolute left-0 top-0 z-30" style={{ top: "var(--spacing-unit)" }}>
 				<svg width="0" height="0">
@@ -118,6 +169,7 @@ export default function Hero() {
 						<div className='absolute right-0 top-0 z-10' id="back-shadow"></div>
 						<div className='w-full h-full z-20' style={{ backgroundImage: "url(/assets/cont/home/hero/sticker-back.webp)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center" }}></div>
 						<div className='absolute right-0 top-0 z-40' id="back-gradient"></div>
+						<div className='absolute right-0 top-0 z-10' id="back-shadow"></div>
 					</div>
 				</div>
 			</div>
